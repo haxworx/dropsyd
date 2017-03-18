4664fbc470c4c491dab1a768f5abc60327b415fb
Al Poole <netstar@gmail.com>
2017-03-18 19:32:32 +0000

    support multiple users and existing in create users

--- adduser.go
+++ adduser.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 	"crypto/sha256"
 )
 
@@ -21,9 +22,37 @@ func main() {
 
 	sum := sha256.Sum256(pass)
 
-	output := fmt.Sprintf("%s:%x\n", string(user), sum)
+	insert_user := fmt.Sprintf("%s:%x\n", string(user), sum)
 
 	os.Mkdir("config", 0755)
+
+	existing := fmt.Sprintf("%s:", user)
+
+	new_user := true
+
+	var output string
+	m, err := os.Open("config/passwd")
+	if err == nil {
+		r = bufio.NewReader(m)
+		for {
+			bytes, err := r.ReadBytes('\n')
+			if err != nil { break }
+	
+			line := string(bytes)
+			if strings.HasPrefix(line, existing) {
+				output = output + insert_user
+				new_user = false
+			} else {
+				output = output + line
+			} 
+		}
+
+		m.Close()	
+	} 
+
+	if (new_user) {
+		output += insert_user
+	}
 
 	f, _ := os.Create("config/passwd")
 	f.Write([]byte(output))
